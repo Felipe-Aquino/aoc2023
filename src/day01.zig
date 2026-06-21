8db0aa7d3c753c37e0b3078feacf4f83a7026621
Felipe-Aquino <felipeaqfer@gmail.com>
2026-06-20 23:34:05 -0300

    solution for day01 part 1

--- src/day01.zig
+++ src/day01.zig
@@ -1,17 +1,38 @@
 const std = @import("std");
 
-const MAX_FILE_SIZE: usize = 10 * 1024 * 1024;
+fn read_file(gpa: std.mem.Allocator, path: []const u8) ![]u8 {
+    const MAX_FILE_SIZE: usize = 10 * 1024 * 1024;
+    return std.fs.cwd().readFileAlloc(gpa, path, MAX_FILE_SIZE);
+}
 
-fn part1() !void {
-    const gpa = std.heap.page_allocator;
-    const file_path = "./inputs/day01-example.txt";
-    const content = try std.fs.cwd().readFileAlloc(gpa, file_path, MAX_FILE_SIZE);
-    defer gpa.free(content);
+fn part1(content: []u8) !void {
+    var iter = std.mem.splitSequence(u8, content, "\n");
 
-    std.debug.print("Hellow!\n", .{});
-    std.debug.print("File content: {s}!\n{s}", .{file_path, content});
+    var sum: usize = 0;
+
+    while (iter.next()) |line| {
+        if (line.len > 0) { 
+            const idx1 = std.mem.indexOfAny(u8, line, "0123456789").?;
+            const idx2 = std.mem.lastIndexOfAny(u8, line, "0123456789").?;
+
+            const n = 10 * @as(usize, @intCast(line[idx1] - '0'))
+                         + @as(usize, @intCast(line[idx2] - '0'));
+
+            std.debug.print("'{s}' -> {}\n", .{line, n});
+            sum += n;
+        }
+    }
+
+    std.debug.print("sum = {}\n", .{sum});
 }
 
 pub fn main() !void {
-    try part1();
+    // const file_path = "./inputs/day01-example.txt";
+    const file_path = "./inputs/day01.txt";
+
+    const gpa = std.heap.page_allocator;
+    const content = try read_file(gpa, file_path);
+    defer gpa.free(content);
+
+    try part1(content);
 }
